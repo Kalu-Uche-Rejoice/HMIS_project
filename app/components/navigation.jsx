9147d3922235ac2411e45e32997e28c2e02d55ec
Kalu-Uche Rejoice <rejoicekaluuche@gmail.com>
2024-04-17 17:31:36 +0200

    third commit

---
+++ app/components/navigation.jsx
@@ -0,0 +1,10 @@
+function Navigation() {
+    return(
+        <nav>
+            <ul>
+                <li>sign in</li>
+            </ul>
+        </nav>
+    )
+}
+export default Navigation
---
+++ app/components/records.jsx
@@ -0,0 +1,68 @@
+function RecordForm() {
+    return(
+        <form>
+            <label htmlFor="FirstName">
+                First Name
+            <input name="FirstName" type="text" placeholder="Insert Patients first name" required></input>
+            </label>
+            <label htmlFor="OtherName">
+                Other Name
+            <input name="OtherName" type="text" placeholder="Insert Patients other name"></input>
+            </label>
+            <label htmlFor="LastName">
+                Last Name
+            <input name="LastName" type="text" placeholder="Insert Patients Last name" required></input>
+            </label>
+            <label htmlFor="Age">
+                Age
+            <input name="Age" type="number" required></input>
+            </label>
+            <label htmlFor="DOB">
+                Date of Birth
+            <input name="DOB" type="date" required></input>
+            </label>
+            <label htmlFor="passport">
+                Upload passport
+            <input name="passport" type="image"></input>
+            </label>
+            <label htmlFor="gen1">
+            <input name="gen1" type="radio"></input>
+            Female
+            </label>
+            <label htmlFor="gen2">
+            <input name="gen2" type="radio"></input>
+            Male
+            </label>
+
+            <label htmlFor="State of origin">
+                <select name="State of Origin">
+                    <option value="Abia"> Abia</option>
+                </select>
+            </label>
+
+            <label htmlFor="Marital Status">
+                <select name="Marital Status">
+                    <option value="Abia"> Single</option>
+                    <option value="Abia"> Married</option>
+                    <option value="Abia"> Divorced</option>
+                </select>
+            </label>
+
+            <label htmlFor="occupation">
+                Occupation
+            <input name="Occupation" type="text" placeholder="Insert Patients Occupation" required></input>
+            </label>
+            <label htmlFor="phoneNumber">
+                Phone Number
+            <input name="phoneNumber" type="text" placeholder="Insert Patients Phone Numer" required></input>
+            </label>
+
+            <label htmlFor="address">
+                Residential Address
+            <input name="address" type="text" placeholder="Insert Patients Address" required></input>
+            </label>
+        </form>
+    )
+}
+
+export default RecordForm
